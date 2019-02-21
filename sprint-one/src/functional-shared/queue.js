7d72106442c52f84841956bc942363e3421edefd
Harrison Wu <harrisonywu@gmail.com>
2019-02-20 21:46:36 -0800

    Pass all queue and stack tests for functional-shared

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,38 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var extend = function(obj, methods) {
+  for (var key in methods) {
+    obj[key] = methods[key];
+  }
 };
 
-var queueMethods = {};
+var Queue = function() {
+  var someInstance = {};
+  someInstance.nextKeyInsert = 1;
+  someInstance.nextKeyRemove = 1;
+  someInstance.storage = {};
+  extend(someInstance, queueMethods);
+  return someInstance;
+};
+
+var queueMethods = {
+  enqueue: function(value) {
+    this.storage[this.nextKeyInsert] = value;
+    this.nextKeyInsert++;
+  },
+  dequeue: function() {
+    if (this.storage[this.nextKeyRemove]) { //change the if case so that it selects the nextKeyRemove in storage.
+      var dequeuedValue = this.storage[this.nextKeyRemove];
+      delete this.storage[this.nextKeyRemove];
+	  }
+	  this.nextKeyRemove++;
+	  return dequeuedValue;
+	},
+	size: function() {
+		var keyCount = 0;
+		for (var key in this.storage) {
+	    	keyCount++;
+		 }
+		 return keyCount;
+	}
+};
 
 
