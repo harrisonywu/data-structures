2bebde29a3bf3ab8b37c793675fa016a7811ca4a
harrisonywu <harrisonywu@gmail.com>
2019-02-23 18:21:52 -0800

    Implement mostly working tree.contains

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -8,6 +8,7 @@ var Tree = function(value) {
 
   //implement an extend function invocation so that..
   // we place treeMethods' keys within our var Tree
+  _.extend(newTree,treeMethods);
 
   return newTree;
 };
@@ -18,13 +19,32 @@ treeMethods.addChild = function(value) {
   //inputs: any value
   // output: no output
   var newChild = Tree(value);
-  this.newTree.children.push(newChild);  
+  this.children.push(newChild); 
+  //tree.newTree
+  // this binds to an instance of Tree (tree.addChild(5)) 
 };
 
 treeMethods.contains = function(target) {
+  
+  
+  // if the value of target is equal to current node's value
+  //     return true in this case
+  // if it is not true
+  //    if the length of children is greater than 0
+  //    we should go through the list of children of child node
+  //       we can use recursion of this function to use this process with the node child
+  // otherwise return false
+  
+  if(target === this.value){
+    return true;
+  }
+   if(this.children.length > 0){
+     for(var i=0;i < this.children.length;i++){
+      return this.children[i].contains(target);
+     }
+   }
+   return false;
 };
-
-
 
 /*
  * Complexity: What is the time complexity of the above functions?
